dfc9814049239e9d24005da8763de7dde38e7f0e
adrrob1002 <adrianrobinson101@gmail.com>
2023-04-23 13:47:09 +0300

    feat: add scene controller

---
+++ src/client/controllers/scene.ts
@@ -0,0 +1,35 @@
+import { Controller, OnStart } from "@flamework/core";
+import Signal from "@rbxts/signal";
+import { ClientStore } from "client/rodux/rodux";
+import { Scene } from "shared/enums";
+
+@Controller({})
+export class SceneController implements OnStart {
+	public OnSceneChanged = new Signal<(newScene: Scene, oldScene?: Scene) => void>()
+
+	public onStart(): void {
+		this.onSceneChanged(ClientStore.getState().gameState.currentScene);
+
+		ClientStore.changed.connect((newState, oldState) => {
+			if (newState.gameState.currentScene !== oldState.gameState.currentScene) {
+				this.onSceneChanged(newState.gameState.currentScene, oldState.gameState.currentScene);
+			}
+		});
+	}
+
+	public getSceneEnteredSignal(scene: Scene): Signal {
+		const sceneEntered = new Signal();
+		this.OnSceneChanged.Connect((newScene, oldScene) => {
+			if (newScene === scene) sceneEntered.Fire();
+		});
+		return sceneEntered;
+	}
+
+	public setScene(newScene: Scene) {
+		ClientStore.dispatch({ type: "SetScene", newScene });
+	}
+
+	private onSceneChanged(newScene: Scene, oldScene?: Scene) {
+		this.OnSceneChanged.Fire(newScene, oldScene);
+	}
+}
