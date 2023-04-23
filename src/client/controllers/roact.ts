9503943692af3ba50583069efa0fbd42d8b6cfa0
AlphaRunic <roleyspersonal@gmail.com>
2023-04-23 03:40:38 -0700

    chore: convert loadscreen to Roact

--- src/client/controllers/roact.ts
+++ src/client/controllers/roact.ts
@@ -2,12 +2,14 @@ import { Controller, OnInit } from "@flamework/core";
 import Roact from "@rbxts/roact";
 
 import { UI } from "client/ui";
-import { Menu } from "client/roact/menu";
+import { LoadScreen } from "client/roact/loadscreen";
 import { HUD } from "client/roact/hud";
+import { Menu } from "client/roact/menu";
 
 @Controller()
 export class RoactController implements OnInit {
   public onInit(): void {
+    Roact.mount(LoadScreen, UI.container);
     Roact.mount(HUD, UI.container);
     Roact.mount(Menu, UI.container);
   }
