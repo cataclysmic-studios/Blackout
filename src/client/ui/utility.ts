5f121e434ada6bec32044452c9bac694014bc7f8
AlphaRunic <roleyspersonal@gmail.com>
2023-04-24 17:48:07 -0700

    chore: remove ui/utility.ts

--- src/client/ui/utility.ts
+++
@@ -1,8 +0,0 @@
-import { Children, Element, PropsWithChildren } from "@rbxts/roact";
-
-export function getChildren<P = {}>(props: PropsWithChildren<P>): Element[] {
-  const childrenMap = props[Children];
-  const children: Element[] = [];
-  childrenMap?.forEach(e => children.push(e));
-  return children;
-}
