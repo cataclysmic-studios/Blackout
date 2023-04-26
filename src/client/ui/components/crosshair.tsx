fb8aa9bf9958fca248c656bb64ec594f66c3da18
AlphaRunic <roleyspersonal@gmail.com>
2023-04-26 11:03:00 -0700

    feat: add stroke to crosshair

--- src/client/ui/components/crosshair.tsx
+++ src/client/ui/components/crosshair.tsx
@@ -21,28 +21,40 @@ export function Crosshair(props: Props) {
         BackgroundColor3={Color3.fromRGB(255, 255, 255)}
         BorderSizePixel={0}
         Position={new UDim2(0.5, 0, 0, 0)}
-        Size={new UDim2(0, 2, 0, 12)} />
+        Size={new UDim2(0, 2, 0, 12)}
+      >
+        <uistroke />
+      </frame>
       <frame
         Key="B"
         AnchorPoint={new Vector2(0.5, 1)}
         BackgroundColor3={Color3.fromRGB(255, 255, 255)}
         BorderSizePixel={0}
         Position={new UDim2(0.5, 0, 1, 0)}
-        Size={new UDim2(0, 2, 0, 12)} />
+        Size={new UDim2(0, 2, 0, 12)}
+      >
+        <uistroke />
+      </frame>
       <frame
         Key="L"
         AnchorPoint={new Vector2(0, 0.5)}
         BackgroundColor3={Color3.fromRGB(255, 255, 255)}
         BorderSizePixel={0}
         Position={new UDim2(0, 0, 0.5, 0)}
-        Size={new UDim2(0, 12, 0, 2)} />
+        Size={new UDim2(0, 12, 0, 2)}
+      >
+        <uistroke />
+      </frame>
       <frame
         Key="R"
         AnchorPoint={new Vector2(1, 0.5)}
         BackgroundColor3={Color3.fromRGB(255, 255, 255)}
         BorderSizePixel={0}
         Position={new UDim2(1, 0, 0.5, 0)}
-        Size={new UDim2(0, 12, 0, 2)} />
+        Size={new UDim2(0, 12, 0, 2)}
+      >
+        <uistroke />
+      </frame>
       <uiaspectratioconstraint />
     </frame>
   );
