51ce7cebc2bb0a9848e5626743304eb960ff43fc
AlphaRunic <roleyspersonal@gmail.com>
2023-04-26 12:19:22 -0700

    chore: remove client destructible

--- src/server/components/destructible.ts
+++ src/server/components/destructible.ts
@@ -6,6 +6,9 @@ interface Attributes { }
 @Component({ tag: "Destructible" })
 export class Destructible extends BaseComponent<Attributes, BasePart> {
   public addBulletHole(hit: CFrame, bulletSize: Vector3): void {
-    Events.replicateBulletHole.broadcast(hit, bulletSize);
+    const negative = new Instance("NegateOperation");
+    negative.CFrame = hit;
+    negative.Size = bulletSize.mul(2);
+    this.instance.UnionAsync([negative], Enum.CollisionFidelity.Hull);
   }
 }
