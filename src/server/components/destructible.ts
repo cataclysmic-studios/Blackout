a776aa22ce5387e50cbfe28b679f7df63c4144f7
AlphaRunic <roleyspersonal@gmail.com>
2023-04-26 12:07:17 -0700

    feat: broadcast replicateBulletHole from destructible server

--- src/server/components/destructible.ts
+++ src/server/components/destructible.ts
@@ -1,10 +1,11 @@
 import { Component, BaseComponent } from "@flamework/components";
+import { Events } from "server/network";
 
 interface Attributes { }
 
 @Component({ tag: "Destructible" })
 export class Destructible extends BaseComponent<Attributes, BasePart> {
   public addBulletHole(hit: CFrame, bulletSize: Vector3): void {
-
+    Events.replicateBulletHole.broadcast(hit, this.instance.Material, this.instance.Color, bulletSize);
   }
 }
